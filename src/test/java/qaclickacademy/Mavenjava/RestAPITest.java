e837c04773ea6873e65844fd227e89fca29cacd6
“valya13” <vavb1117@gmail.com>
2020-12-20 21:32:06 -0600

    new commit

--- src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -9,7 +9,9 @@ public class RestAPITest {
 	@Test
 	public void PostJira()
 	{
-		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
 	}
 	
 	@Test
